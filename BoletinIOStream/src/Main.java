458da8a5016a47f6f8f7c8201d40190b45ced133
JPA-Castelao <jpinedaarenas@danielcastelao.org>
2025-10-16 13:19:19 +0200

    Add files via upload

---
+++ BoletinIOStream/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public void crearArquivo(String ruta) {
+        File arquivo = new File(ruta);
+        String nomes = "Tanjiro\nNezuko\nMuzan";
+        File
+        if (!arquivo.exists()) {
+
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Path path = Paths.get("dam", "ad", "BoletinIOStream", "DemonSlayer.txt");
+        String ruta = path.toString();
+
+    }
+}
